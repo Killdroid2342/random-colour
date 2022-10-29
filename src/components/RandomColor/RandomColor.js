98d71a5aef72b28e61b63eaf5c768ace78da7f1a
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-29 17:11:24 +0100

    "centered everything"

--- src/components/RandomColor/RandomColor.js
+++ src/components/RandomColor/RandomColor.js
@@ -44,42 +44,44 @@ const RandomColor = () => {
       <h1 className='text-white font-bold font-Verdana text-center mt-60 text-4xl'>
         Random Color Generator
       </h1>
-      <div
-        className='border-2 border-zinc-500 flex flex-col h-52 w-80 text-zinc-500 ml-80 font-bold font-Verdana rounded-md mt-20'
-        style={{
-          backgroundColor: color,
-        }}
-      >
-        <div className='text-2xl ml-28 mt-10'>{color}</div>
-        <button
-          onClick={Gen}
-          className='font-Verdana w-44 mt-5 ml-20 bg-transparent rounded-3xl text-zinc-500 border-2 border-zinc-600 font-bold cursor-pointer text-base'
+      <div className=' flex flex-col items-center justify-center'>
+        <div
+          className='border-2 border-zinc-500 flex flex-col h-52 w-80 text-zinc-500 font-bold font-Verdana rounded-md mt-20'
+          style={{
+            backgroundColor: color,
+          }}
         >
-          Click For New Color
-        </button>
-        <button
-          className='border-2 border-zinc-600 rounded-md mt-4 w-52 ml-16 pb-1 pt-1'
-          onClick={() => navigator.clipboard.writeText(color)}
-        >
-          Copy Color
-        </button>
-      </div>
-      <div className='border-2 border-zinc-500 h-14 w-80 ml-80 mt-4 rounded-md text-zinc-500'>
-        <button className='btn-color' onClick={blackClick}>
-          Black
-        </button>
-        <button className='btn-color' onClick={whiteClick}>
-          White
-        </button>
-        <button className='btn-color' onClick={blueClick}>
-          Blue
-        </button>
-        <button className='btn-color' onClick={redClick}>
-          Red
-        </button>
-        <button className='btn-color' onClick={greenClick}>
-          Green
-        </button>
+          <div className='text-2xl ml-28 mt-10'>{color}</div>
+          <button
+            onClick={Gen}
+            className='font-Verdana w-44 mt-5 ml-20 bg-transparent rounded-3xl text-zinc-500 border-2 border-zinc-600 font-bold cursor-pointer text-base'
+          >
+            Click For New Color
+          </button>
+          <button
+            className='border-2 border-zinc-600 rounded-md mt-4 w-52 ml-16 pb-1 pt-1'
+            onClick={() => navigator.clipboard.writeText(color)}
+          >
+            Copy Color
+          </button>
+        </div>
+        <div className='border-2 border-zinc-500 h-14 w-80 mt-4 rounded-md text-zinc-500'>
+          <button className='btn-color' onClick={blackClick}>
+            Black
+          </button>
+          <button className='btn-color' onClick={whiteClick}>
+            White
+          </button>
+          <button className='btn-color' onClick={blueClick}>
+            Blue
+          </button>
+          <button className='btn-color' onClick={redClick}>
+            Red
+          </button>
+          <button className='btn-color' onClick={greenClick}>
+            Green
+          </button>
+        </div>
       </div>
     </React.Fragment>
   );
