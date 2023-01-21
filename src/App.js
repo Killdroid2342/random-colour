ab8af29ea5691dde720125b2fc7e30fb9512ebb3
Haroon Khan <HaroonKhan120704@gmail.com>
2023-01-21 00:02:04 +0000

    added another import and removed jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ColorButtons from './components/ColorButtons/ColorButtons';
+import Main from './components/Main/Main';
 
 document.title = 'Random Color Generator';
 
@@ -48,32 +49,13 @@ function App() {
         Random Color Generator
       </h1>
       <div className=' flex flex-col items-center justify-center'>
-        <div
-          className='border-2 border-zinc-500 flex flex-col h-52 w-80 text-zinc-500 font-bold font-Verdana rounded-md mt-20'
-          style={{
-            backgroundColor: color,
-          }}
-        >
-          <div className='text-2xl ml-28 mt-10'>{color}</div>
-          <button
-            onClick={Gen}
-            className='font-Verdana w-44 mt-5 ml-20 bg-transparent rounded-3xl text-zinc-500 border-2 border-zinc-600 font-bold cursor-pointer text-base'
-          >
-            Click For New Color
-          </button>
-          <button
-            className='border-2 border-zinc-600 rounded-md mt-4 w-52 ml-16 pb-1 pt-1'
-            onClick={() => navigator.clipboard.writeText(color)}
-          >
-            Copy Color
-          </button>
-        </div>
+        <Main color={color} Gen={Gen} />
         <ColorButtons
           blackClick={blackClick}
           whiteClick={whiteClick}
           blueClick={blueClick}
           redClick={redClick}
-          greenClick={redClick}
+          greenClick={greenClick}
         />
       </div>
     </React.Fragment>
